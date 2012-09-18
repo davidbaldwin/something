93ea3b0743fc5af9e7c43400e457492edca44e52
David Baldwin <david_o_baldwin@yahoo.com>
2012-09-18 08:26:12 +0100

    Got something working

--- src/_something_test.js
+++ src/_something_test.js
@@ -4,8 +4,7 @@ var something = require("./something.js");
 
 
 exports.test_Add1to2 = function(test) {
-	something.doSomething(1, 2, function(numeral) {
-		test.equal(numeral, 3);
-		test.done();
-	});
+	var actual = something.doSomething(1, 2);
+    test.equal(actual, 3);
+    test.done();
 };
